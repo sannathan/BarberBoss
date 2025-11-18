684bcac9a7de15ba10c94ed85041e92af7f76890
sannathan <nathansantos1713@gmail.com>
2025-11-18 13:38:25 -0300

    feat: creating dbcontext class
    That class is used to mapping Billing entity to ou Billing table, connect our aplication to db and db operations

---
+++ src/BarberBoss.Infraestructure/DataAccess/BarberBossDbContext.cs
@@ -0,0 +1,11 @@
+﻿using BarberBoss.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarberBoss.Infraestructure.DataAccess
+{
+    internal class BarberBossDbContext : DbContext
+    {
+        public BarberBossDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Billing> Billings { get; set; }
+    }
+}
