65d4cab444bbc8430c7e2894a1da6c5720a4dd68
sannathan <nathansantos1713@gmail.com>
2025-11-18 13:42:30 -0300

    feat: adding UnitOfWork to ensure our operations is saved properly

---
+++ src/BarberBoss.Infraestructure/DataAccess/UnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using BarberBoss.Domain.Repositories;
+using System.Runtime.CompilerServices;
+
+namespace BarberBoss.Infraestructure.DataAccess
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        private readonly BarberBossDbContext _dbContext;
+        public UnitOfWork(BarberBossDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task Commit() => await _dbContext.SaveChangesAsync();
+    }
+}
