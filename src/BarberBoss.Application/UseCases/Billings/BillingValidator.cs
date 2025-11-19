206d8f2ed05f7668cacdc16682085cb036d463ee
sannathan <nathansantos1713@gmail.com>
2025-11-19 15:30:58 -0300

    Feat: creating Resource Error messages and creating Validate class to validate our request

---
+++ src/BarberBoss.Application/UseCases/Billings/BillingValidator.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper.Configuration;
+using BarberBoss.Communication.Requests;
+using BarberBoss.Exception;
+using FluentValidation;
+
+namespace BarberBoss.Application.UseCases.Billings
+{
+    public class BillingValidator : AbstractValidator<RequestBillingJson>
+    {
+        public BillingValidator()
+        {
+            RuleFor(billing => billing.Date).LessThanOrEqualTo(DateOnly.FromDateTime(DateTime.UtcNow)).WithMessage(ResourceErrorMessages.DATE_CANNOT_BE_FUTURE);
+            RuleFor(billing => billing.BarberName).MinimumLength(2).WithMessage(ResourceErrorMessages.BARBER_NAME_MUST_BE_GREATHER_THAN_TWO_LETTERS);
+            RuleFor(billing => billing.BarberName).MaximumLength(80).WithMessage(ResourceErrorMessages.BARBER_NAME_MUST_BE_LESS_THAN_EIGHTY_LETTERS);
+            RuleFor(billing => billing.ClientName).MinimumLength(2).WithMessage(ResourceErrorMessages.CLIENT_NAME_MUST_BE_GREATHER_THAN_TWO_LETTERS);
+            RuleFor(billing => billing.ClientName).MaximumLength(120).WithMessage(ResourceErrorMessages.CLIENT_NAME_MUST_BE_LESS_THAN_ONE_HUNDRED_TWENTY_LETTERS);
+            RuleFor(billing => billing.ServiceName).MinimumLength(2).WithMessage(ResourceErrorMessages.SERVICE_NAME_MUST_BE_GREATHER_THAN_TWO_LETTERS);
+            RuleFor(billing => billing.ServiceName).MaximumLength(120).WithMessage(ResourceErrorMessages.SERVICE_NAME_MUST_BE_LESS_THAN_ONE_HUNDRED_TWENTY_LETTERS);
+            RuleFor(billing => billing.Amount).GreaterThanOrEqualTo(0).WithMessage(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
+            RuleFor(billing => billing.PaymentMethod).IsInEnum().WithMessage(ResourceErrorMessages.PAYMENT_METHOD_INVALID);
+            RuleFor(billing => billing.Status).IsInEnum().WithMessage(ResourceErrorMessages.PAYMENT_METHOD_INVALID);
+            RuleFor(billing => billing.CreatedAt).LessThanOrEqualTo(DateTime.UtcNow).WithMessage(ResourceErrorMessages.CREATED_AT_CANNOT_BE_FUTURE);
+            RuleFor(billing => billing.UpdatedAt).LessThanOrEqualTo(DateTime.UtcNow).WithMessage(ResourceErrorMessages.UPDATED_AT_CANNOT_BE_FUTURE);
+        }
+
+    }
+}
--- src/BarberBoss.Application/UseCases/Billings/Register/RegisterBillingUseCase.cs
+++ src/BarberBoss.Application/UseCases/Billings/Register/RegisterBillingUseCase.cs
@@ -1,12 +1,45 @@
-﻿using BarberBoss.Communication.Requests;
+﻿using AutoMapper;
+using BarberBoss.Communication.Requests;
 using BarberBoss.Communication.Responses;
+using BarberBoss.Domain.Repositories;
+using BarberBoss.Domain.Repositories.Billings;
+using Microsoft.IdentityModel.Tokens;
 
 namespace BarberBoss.Application.UseCases.Billings.Register
 {
     public class RegisterBillingUseCase : IRegisterBillingUseCase
     {
-        Task<ReponseRegisterBillingJson> IRegisterBillingUseCase.Execute(RequestBillingJson request)
+        private readonly IBillingsWriteOnlyRepository _repository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public RegisterBillingUseCase(IBillingsWriteOnlyRepository repository, IUnitOfWork unitOfWork, IMapper mapper)
         {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public async Task<ReponseRegisterBillingJson> IRegisterBillingUseCase.Execute(RequestBillingJson request)
+        {
+            Validate(request);
+
+             
+
+        }
+
+        private void Validate(RequestBillingJson request)
+        {
+            var validator = new BillingValidator();
+
+            var result = validator.Validate(request);
+
+            if(result.IsValid == false)
+            {
+                var errorMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
+
+                throw new ErrorOnValidationException(errorMessages);
+            }
+
         }
     }
 }
