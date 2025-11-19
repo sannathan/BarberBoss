86768a3f98029b0f5bd9aa047c6feb08e05fee59
sannathan <nathansantos1713@gmail.com>
2025-11-19 13:24:16 -0300

    Refactor project structure and add DI support
    Updated folder structure in `BarberBoss.Infraestructure.csproj` to include `DataAccess\Repositories\`. Added NuGet packages for dependency injection (`Microsoft.Extensions.DependencyInjection.Abstractions`) and MySQL support (`Pomelo.EntityFrameworkCore.MySql`).
    
    Refactored `DependencyInjectionExtension`:
    - Made the class `public static` for broader accessibility.
    - Added `AddInfraestructure` method to configure services.
    - Introduced `AddRepositories` to register `IUnitOfWork` with `UnitOfWork`.
    - Added `AddDbContext` with a placeholder for database connection configuration.
    
    Removed unused `using` directives and added necessary ones for new functionality.

--- src/BarberBoss.Infraestructure/DependencyInjectionExtension.cs
+++ src/BarberBoss.Infraestructure/DependencyInjectionExtension.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BarberBoss.Domain.Repositories;
+using BarberBoss.Infraestructure.DataAccess;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
 
 namespace BarberBoss.Infraestructure
 {
-    internal class DependencyInjectionExtension
+    public static class DependencyInjectionExtension
     {
+        public static void AddInfraestructure(this IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+
+        private static void AddRepositories(IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+
+        private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Connection");
+        }
     }
 }
