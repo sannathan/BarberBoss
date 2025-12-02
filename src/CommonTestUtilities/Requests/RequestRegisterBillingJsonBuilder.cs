583adc74532343db1e1673f42e7c28b2c656329e
sannathan <nathansantos1713@gmail.com>
2025-12-02 15:31:42 -0300

    feat: adding RequestBuilder to mock requests data

---
+++ src/CommonTestUtilities/Requests/RequestRegisterBillingJsonBuilder.cs
@@ -0,0 +1,23 @@
+﻿using BarberBoss.Communication.Enums;
+using BarberBoss.Communication.Requests;
+using Bogus;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CommonTestUtilities.Requests
+{
+    public class RequestRegisterBillingJsonBuilder
+    {
+        public static RequestBillingJson Build()
+        {
+            return new Faker<RequestBillingJson>()
+                .RuleFor(r => r.Date, faker => DateOnly.FromDateTime(faker.Date.Recent(30)))
+                .RuleFor(r => r.BarberName, faker => faker.Name.FirstName())
+                .RuleFor(r => r.ClientName, faker => faker.Name.FirstName())
+                .RuleFor(r => r.ServiceName, faker => faker.Commerce.ProductName())
+                .RuleFor(r => r.Amount, faker => faker.Finance.Amount(min:1, max:100))
+                .RuleFor(r => r.PaymentMethod, faker => faker.Random.Enum<PaymentMethod>())
+                .RuleFor(r => r.Status, faker => faker.Random.Enum<Status>())
+                .RuleFor(r => r.Notes, faker => faker.Commerce.ProductDescription());
+        }
+    }
+}
